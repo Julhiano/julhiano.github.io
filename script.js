7c3cc2a39513b34b558b25aba507cfe0720afd32
Julhiano <julhianopnogueira@hotmail.com>
2023-02-09 11:29:45 -0300

    Corrigido troca de tema

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function toggleMode() {
-  const html = document.documentElement
+  const html = document.documentElement;
 
- /* if (html.classList.contains("light")) {
+  if (html.classList.contains("light")) {
     html.classList.remove("light")
   } else {
     html.classList.add("light")
-  }*/
+  }
 
-  html.classLists.toggleMode('light')
+  // html.classList.toggleMode("light")
 }
